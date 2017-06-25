f362c7339411dcb87898d6ffa933c7551d5e5e89
Daniel O'Connell <dan.ben.oconnell@gmail.com>
2017-06-26 08:09:01 +1000

    Following second video by sentdex

--- mnist.py
+++ mnist.py
@@ -15,37 +15,37 @@ y = tf.placeholder("float")
 
 
 def neural_network(data):
-    hidden_l1 = {'weights':tf.Variable(tf.random_normal([784, number_nodes_l1])),
-                      'biases':tf.Variable(tf.random_normal([number_nodes_l1]))}
+    hidden_l1 = {'weights': tf.Variable(tf.random_normal([784, number_nodes_l1])),
+                 'biases': tf.Variable(tf.random_normal([number_nodes_l1]))}
 
-    hidden_l2 = {'weights':tf.Variable(tf.random_normal([number_nodes_l1, number_nodes_l2])),
-                      'biases':tf.Variable(tf.random_normal([number_nodes_l2]))}
+    hidden_l2 = {'weights': tf.Variable(tf.random_normal([number_nodes_l1, number_nodes_l2])),
+                 'biases': tf.Variable(tf.random_normal([number_nodes_l2]))}
 
-    hidden_l3 = {'weights':tf.Variable(tf.random_normal([number_nodes_l2, number_nodes_l3])),
-                      'biases':tf.Variable(tf.random_normal([number_nodes_l3]))}
+    hidden_l3 = {'weights': tf.Variable(tf.random_normal([number_nodes_l2, number_nodes_l3])),
+                 'biases': tf.Variable(tf.random_normal([number_nodes_l3]))}
 
-    output_layer = {'weights':tf.Variable(tf.random_normal([number_nodes_l3, number_classes])),
-                    'biases':tf.Variable(tf.random_normal([number_classes])),}
+    output_layer = {'weights': tf.Variable(tf.random_normal([number_nodes_l3, number_classes])),
+                    'biases': tf.Variable(tf.random_normal([number_classes]))}
 
-
-    l1 = tf.add(tf.matmul(data,hidden_l1['weights']), hidden_l1['biases'])
+    l1 = tf.add(tf.matmul(data, hidden_l1['weights']), hidden_l1['biases'])
     l1 = tf.nn.relu(l1)
 
-    l2 = tf.add(tf.matmul(l1,hidden_l2['weights']), hidden_l2['biases'])
+    l2 = tf.add(tf.matmul(l1, hidden_l2['weights']), hidden_l2['biases'])
     l2 = tf.nn.relu(l2)
 
-    l3 = tf.add(tf.matmul(l2,hidden_l3['weights']), hidden_l3['biases'])
+    l3 = tf.add(tf.matmul(l2, hidden_l3['weights']), hidden_l3['biases'])
     l3 = tf.nn.relu(l3)
 
-    output = tf.matmul(l3,output_layer['weights']) + output_layer['biases']
+    output = tf.matmul(l3, output_layer['weights']) + output_layer['biases']
 
     return output
 
+
 def train_the_beast(x):
     prediction = neural_network(x)
-    cost = tf.reduce_mean( tf.nn.softmax_cross_entropy_with_logits(logits=prediction, labels=y) )
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=prediction, labels=y))
     optimizer = tf.train.AdamOptimizer().minimize(cost)
-    
+
     number_epochs = 10
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
@@ -62,6 +62,7 @@ def train_the_beast(x):
         correct = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1))
 
         accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
-        print('Accuracy:',accuracy.eval({x:mnist.test.images, y:mnist.test.labels}))
+        print('Accuracy:', accuracy.eval({x: mnist.test.images, y: mnist.test.labels}))
+
 
 train_the_beast(x)
